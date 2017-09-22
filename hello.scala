e522e014edfb0e79747a11d601f147adfd009086
amitanalyste <amitanalyste@gmail.com>
2017-09-22 02:08:09 -0700

    uploading list and usage

---
+++ hello.scala
@@ -0,0 +1,14 @@
+//println("Hello, world, from a script!")
+//println("Hello, " + args(1) +  ' ' + args(0) +"!")
+/*var i = 0
+while (i < args.length){
+	if(i != 0) print(" ")
+	print(args(i))
+	i += 1
+}
+println()
+*/
+//args.foreach(arg => println(arg))
+//args.foreach(println)
+for (arg <- args)
+	println(arg)
